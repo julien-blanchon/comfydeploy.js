aac0a05fafb9ab16ce079da75e658e458b74ba8b
Blanchon <julien@blanchon.cc>
2024-03-12 16:30:21 +0100

    Change to `live_status: z.string().nullable()`

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const runOutputTypes = z.object({
       data: z.any(),
     }),
   ),
-  live_status: z.string().optional(),
+  live_status: z.string().nullable(),
   progress: number().default(0)
 });
 
